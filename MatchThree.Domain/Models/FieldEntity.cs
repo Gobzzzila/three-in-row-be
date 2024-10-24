86ef74619c4f59de37403a652d1788d527b5a38b
Gobzzzila <23122297+Gobzzzila@users.noreply.github.com>
2024-10-24 23:33:39 +0300

    Get field endpoint

--- MatchThree.Domain/Models/FieldEntity.cs
+++ MatchThree.Domain/Models/FieldEntity.cs
@@ -5,5 +5,6 @@ namespace MatchThree.Domain.Models;
 public class FieldEntity
 {
     public long Id { get; set; }
+    public int[][] Field { get; set; } = [];
     public FieldLevels FieldLevel { get; set; }
 }
--- MatchThree/Controllers/FieldController.cs
+++ MatchThree/Controllers/FieldController.cs
@@ -8,19 +8,39 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace MatchThree.API.Controllers;
 
 [ApiController]
-[Route("api/v1/field-elements")]
+[Route("api/v1/field")]
 public class FieldController(IUpdateFieldService updateFieldService,
+    IReadFieldService readFieldService,
     ITransactionService transactionService)
 {
     private readonly IUpdateFieldService _updateFieldService = updateFieldService;
+    private readonly IReadFieldService _readFieldService = readFieldService;
     private readonly ITransactionService _transactionService = transactionService;
 
     /// <summary>
     /// Upgrade field by user identifier 
     /// </summary>
+    [HttpGet("{userId:long}")]
+    [Authorize(Policy = AuthenticationConstants.UserIdPolicy)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int[][]))]
+    [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status402PaymentRequired, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(ProblemDetails))]
+    [SwaggerOperation(OperationId = "GetField", Tags = ["Field"])]
+    public async Task<IResult> GetField(long userId, CancellationToken cancellationToken = new())
+    {
+        var fieldEntity = await _readFieldService.GetByUserIdAsync(userId);
+        return Results.Ok(fieldEntity.Field);
+    }
+    
+    /// <summary>
+    /// Upgrade field by user identifier 
+    /// </summary>
     [HttpPost("{userId:long}/upgrade-field", Name = EndpointsConstants.UpgradeFieldEndpointName)]
     [Authorize(Policy = AuthenticationConstants.UserIdPolicy)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status409Conflict, Type = typeof(ProblemDetails))]
     [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ProblemDetails))]
     [ProducesResponseType(StatusCodes.Status403Forbidden, Type = typeof(ProblemDetails))]
@@ -31,6 +51,6 @@ public class FieldController(IUpdateFieldService updateFieldService,
     {
         await _updateFieldService.UpgradeFieldAsync(userId);
         await _transactionService.Commit();
-        return Results.Ok();
+        return Results.NoContent();
     }
 }
